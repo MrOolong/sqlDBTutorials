c59b715f9ac40bbd569f3001d051a0b5ab5eb06e
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-01-15 15:10:51 -0500

    initial commit Cookbook1 and WindowsFormsApp1

---
+++ Cookbook1/Cookbook1/FrmMain.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Cookbook1
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRecipes = new System.Windows.Forms.Label();
+            this.lstRecipes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblRecipes
+            // 
+            this.lblRecipes.AutoSize = true;
+            this.lblRecipes.Location = new System.Drawing.Point(26, 27);
+            this.lblRecipes.Name = "lblRecipes";
+            this.lblRecipes.Size = new System.Drawing.Size(67, 20);
+            this.lblRecipes.TabIndex = 0;
+            this.lblRecipes.Text = "Recipes";
+            this.lblRecipes.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lstRecipes
+            // 
+            this.lstRecipes.FormattingEnabled = true;
+            this.lstRecipes.ItemHeight = 20;
+            this.lstRecipes.Location = new System.Drawing.Point(30, 50);
+            this.lstRecipes.Name = "lstRecipes";
+            this.lstRecipes.Size = new System.Drawing.Size(120, 84);
+            this.lstRecipes.TabIndex = 1;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 448);
+            this.Controls.Add(this.lstRecipes);
+            this.Controls.Add(this.lblRecipes);
+            this.Name = "FrmMain";
+            this.Text = "Cookbook";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRecipes;
+        private System.Windows.Forms.ListBox lstRecipes;
+    }
+}
+
---
+++ Cookbook1/Cookbook1/SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿SELECT * FROM Recipe
+
+select * from Ingredient
+
+SELECT a.Name FROM Ingredient a
+INNER JOIN RecipeIngredient b ON a.Id = b.IngredientId
+WHERE b.RecipeId = 2;
+
+UPDATE Recipe
+SET Name = 'Salad'
+WHERE Id = 1;
+
+INSERT INTO Recipe
+VALUES ('Salad 4', 50, 'Combine even more!');
+
+DELETE FROM Recipe
+WHERE Id = 3;
